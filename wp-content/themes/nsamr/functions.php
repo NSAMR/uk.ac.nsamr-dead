4268c3cfc533b97d7ee144340aae6dfe98070302
Helen Jackson <helen@deckofpandas.co.uk>
2017-01-17 15:43:24 +0000

    Basic setup for enqueueing custom styling with SASS

--- wp-content/themes/nsamr/functions.php
+++ wp-content/themes/nsamr/functions.php
@@ -1,15 +1,24 @@
 <?php
+
+/*-- ENQUEUE STYLES */
 function nsamr_enqueue_styles() {
 
-    $parent_style = 'assan-style';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
+	// Enqueue parent styles	
+    $parent_style = 'assan';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() .'/style.css' );
+    
+	// Enqueue child styles	
+    wp_enqueue_style( 'nsamr',
+        get_stylesheet_directory_uri() .'/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
+    //wp_enqueue_style( 'variables', get_template_directory_uri() . '/css/variables.css' );
+
    }
 
+/*-- CALL THE ENQUEUE FUNCTIONS */
 add_action( 'wp_enqueue_scripts', 'nsamr_enqueue_styles' );
+
 ?>
